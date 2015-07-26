2ec38737c786f0313b08fccbbec5e437525ecf9a
bxstar <yurongsheng@163.com>
2015-07-26 11:08:08 +0800

    addin格式完成

---
+++ CtripAutoXSD/VSConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+using EnvDTE;
+using System.Reflection;
+using System.IO;
+
+
+namespace CtripAutoXSD
+{
+    public partial class VSConfig : UserControl, IDTToolsOptionsPage
+    {
+        public static string ConfigFullName = Assembly.GetExecutingAssembly().Location + ".config";
+
+        public VSConfig()
+        {
+            InitializeComponent();
+        }
+
+        public void GetProperties(ref object PropertiesObject)
+        {
+            return;
+        }
+
+        /// <summary>
+        /// 界面加载完后，加载配置文件内容
+        /// </summary>
+        /// <param name="DTEObject"></param>
+        public void OnAfterCreated(DTE DTEObject)
+        {
+            using (StreamReader sr = new StreamReader(ConfigFullName))
+            {
+                txtCopyright.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+        }
+
+        /// <summary>
+        /// 取消
+        /// </summary>
+        public void OnCancel()
+        {
+            return;
+        }
+
+        /// <summary>
+        /// 帮助
+        /// </summary>
+        public void OnHelp()
+        {
+            return;
+        }
+
+        /// <summary>
+        /// 保存配置内容
+        /// </summary>
+        public void OnOK()
+        {
+            using (StreamWriter sw = new StreamWriter(ConfigFullName))
+            {
+                sw.Write(txtCopyright.Text);
+                sw.Close();
+            }
+        }
+    }
+}
---
+++ CtripAutoXSD/VSConfig.designer.cs
@@ -0,0 +1,70 @@
+﻿namespace CtripAutoXSD
+{
+    partial class VSConfig
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTip = new System.Windows.Forms.Label();
+            this.txtCopyright = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTip
+            // 
+            this.lblTip.AutoSize = true;
+            this.lblTip.Location = new System.Drawing.Point(4, 13);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(77, 12);
+            this.lblTip.TabIndex = 0;
+            this.lblTip.Text = "插件配置信息";
+            // 
+            // txtCopyright
+            // 
+            this.txtCopyright.Location = new System.Drawing.Point(6, 37);
+            this.txtCopyright.Multiline = true;
+            this.txtCopyright.Name = "txtCopyright";
+            this.txtCopyright.Size = new System.Drawing.Size(460, 280);
+            this.txtCopyright.TabIndex = 1;
+            // 
+            // VSConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtCopyright);
+            this.Controls.Add(this.lblTip);
+            this.Name = "VSConfig";
+            this.Size = new System.Drawing.Size(480, 390);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTip;
+        private System.Windows.Forms.TextBox txtCopyright;
+    }
+}
